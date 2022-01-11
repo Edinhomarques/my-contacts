148c9e2df106b71ca2d99fa821857446ad3ff3a9
Rafaela Marques <rafaelabsmarques@gmail.com>
2022-01-11 19:42:02 -0300

    Implement show and delete methods of Contacts

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,10 +1,17 @@
 const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: 'Edson Marques',
     email: 'edson@gmail.com',
+    phone: '11111111',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Rafaela Marques',
+    email: 'rafaela@gmail.com',
     phone: '11111111',
     category_id: v4(),
   },
@@ -13,6 +20,17 @@ const contacts = [
 class ContactRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(contacts.find((contact) => contact.id === id)));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
