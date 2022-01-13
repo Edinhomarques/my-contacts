e8497b504d1ca0f1983934cbb40e8d78e6d02123
Edson Marques <edson_csl@hotmail.com>
2022-01-13 18:12:08 -0300

    implement update method

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -29,7 +29,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This e-mail is already been taken' });
+      return response.status(400).json({ error: 'This e-mail is already is use' });
     }
     // Criar um novo registro
     const contact = await ContactsRepository.create({
@@ -39,8 +39,31 @@ class ContactController {
     return response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Editar um registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already is use' });
+    }
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+    return response.json(contact);
   }
 
   async delete(request, response) {
