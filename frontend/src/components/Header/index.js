7208664f181237446975569b951bdffb2ad22bae
Edson <edson_csl@hotmail.com>
2022-02-13 14:52:33 -0300

    Create Header component

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, InputSearchContainer } from './styles';
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="My Contacts Logo" width={201} />
+      <InputSearchContainer>
+        <input type="text" placeholder="Search by name..." />
+      </InputSearchContainer>
+    </Container>
+  );
+}
