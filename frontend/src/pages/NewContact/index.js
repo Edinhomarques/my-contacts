a788cdbf92bdc9985be4e740653228fdb1bede69
Edson <edson_csl@hotmail.com>
2022-02-16 16:09:23 -0300

    feat(button): create button component

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PagerHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -15,6 +16,7 @@ export default function NewContact() {
         <option value="123">twitter</option>
         <option value="123">Facebook</option>
       </Select>
+      <Button type="button">Cadastrar</Button>
     </>
   );
 }
