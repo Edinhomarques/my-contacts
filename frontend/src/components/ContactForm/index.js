7a18a98d8bc07d3a3b4477b9e51da29b93523880
Edson <edson_csl@hotmail.com>
2022-02-19 14:59:34 -0300

    feat(errors): Add validations at email and name fields

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -1,39 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, ButtonContainer } from './styles';
 
+import isEmailValid from '../../utils/isEmailValid';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 import FormGroup from '../FormGroup';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+
+    if (!e.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Name is required' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'));
+    }
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+
+    if (e.target.value && !isEmailValid(e.target.value)) {
+      const errorAlreadyExists = errors.find((err) => err.field === 'email');
+      if (errorAlreadyExists) {
+        return;
+      }
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'email', message: 'Format e-mail is invalid' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'email'));
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
+    <Form onSubmit={(e) => handleSubmit(e)}>
+      <FormGroup
+        error={errors.find((err) => err.field === 'name') && errors.find((err) => err.field === 'name').message}
+      >
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(e) => handleNameChange(e)}
+          error={errors.find((err) => err.field === 'name')}
+        />
       </FormGroup>
 
       <FormGroup
-        error="Format e-mail is invalid"
+        error={errors.find((err) => err.field === 'email') && errors.find((err) => err.field === 'email').message}
       >
-        <Input placeholder="Email" error />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => handleEmailChange(e)}
+          error={errors.find((err) => err.field === 'email')}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Phone" />
+        <Input
+          placeholder="Phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">category</option>
           <option value="Instagram">Instagram</option>
           <option value="Twitter">Twitter</option>
           <option value="Facebook">Facebook</option>
           <option value="School">School</option>
         </Select>
       </FormGroup>
+
       <ButtonContainer>
         <Button type="submit">{buttonLabel}</Button>
-
       </ButtonContainer>
     </Form>
   );
