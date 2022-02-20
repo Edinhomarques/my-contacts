3fa70c2a5d024cab02bf875cb66f2f6f2ffe2f4a
Edson <edson_csl@hotmail.com>
2022-02-20 16:35:10 -0300

    feat: Implement fetch api to get contacts data of mycontact-api

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
@@ -11,6 +11,18 @@ import {
 } from './styles';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch('http://localhost:3001/contacts');
+      const responseJson = await response.json();
+      setContacts(responseJson);
+    } catch (error) {
+      throw Error(error);
+    }
+  }, []);
+
   return (
     <Container>
       {false && <Modal danger />}
@@ -19,7 +31,10 @@ export default function Home() {
         <input type="text" placeholder="Search by name..." />
       </InputSearchContainer>
       <Header>
-        <strong>3 Contatos</strong>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' Contact' : ' Contacts'}
+        </strong>
         <Link to="/new">New Contact</Link>
       </Header>
       <ListContainer>
@@ -29,65 +44,28 @@ export default function Home() {
             <img src={arrow} alt="Arrow" />
           </button>
         </header>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Name</strong>
-              <small>Category</small>
+        {contacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="info">
+              <div className="contact-name">
+                <strong>{contact.name}</strong>
+                {contact.category_name && <small>{contact.category_name}</small>}
+              </div>
+              <span>{contact.email}</span>
+              <span>{contact.phone}</span>
             </div>
-            <span>ed@gmail.com</span>
-            <span>(79)31313131</span>
-          </div>
 
-          <div className="actions">
-            <Link to="/edit/124">
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Name</strong>
-              <small>Category</small>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={edit} alt="Edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="Delete" />
+              </button>
             </div>
-            <span>ed@gmail.com</span>
-            <span>(79)31313131</span>
-          </div>
+          </Card>
 
-          <div className="actions">
-            <Link to="/edit/123">
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Name</strong>
-              <small>Category</small>
-            </div>
-            <span>ed@gmail.com</span>
-            <span>(79)31313131</span>
-          </div>
-
-          <div className="actions">
-            <Link to="/edit/123">
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
+        ))}
       </ListContainer>
     </Container>
   );
