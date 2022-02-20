8393bb78a4ec78b1881a563cff233f29a11a63dd
Edson <edson_csl@hotmail.com>
2022-02-20 15:40:43 -0300

    feat(cors): add config to headers and methods in cors middleware

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,5 +1,7 @@
 const cors = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
   next();
 };
 
